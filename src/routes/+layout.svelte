51c5af694fe4d7d2baf91384554895e24fa09575
PokeFred <Cedric.paelmke@gmx.de>
2024-02-15 22:30:24 +0100

    clean up darkmode and themes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import '../app.css'
+    import Darkmode from "$stores/darkmode"
+    import Theme from "$stores/theme"
 </script>
 
-<body data-theme="skeleton" class="dark">
+<body data-theme={$Theme} class="{$Darkmode ? 'dark' : 'light'}">
     <div class="top-0 left-0 w-screen h-screen text-surface-900-50-token bg-surface-100-800-token overflow-hidden absolute">
         <!-- sidebar or other stuff... -->
-        <div class="p-4">
+        <main class="p-4">
             <slot />
-        </div>
+        </main>
     </div>
 </body>
