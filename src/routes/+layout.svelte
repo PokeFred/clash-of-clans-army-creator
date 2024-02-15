bbefca8293682d8ca10f90a0437681e3f98203eb
PokeFred <Cedric.paelmke@gmx.de>
2024-02-15 22:27:08 +0100

    add skeletonUI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
     import '../app.css'
 </script>
 
-<slot />
+<body data-theme="skeleton" class="dark">
+    <div class="top-0 left-0 w-screen h-screen text-surface-900-50-token bg-surface-100-800-token overflow-hidden absolute">
+        <!-- sidebar or other stuff... -->
+        <div class="p-4">
+            <slot />
+        </div>
+    </div>
+</body>
